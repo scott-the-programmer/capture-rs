ad3457cef9098229b1054937d7734c65bfeda414
Scott Murray <scott@murray.kiwi>
2023-12-03 16:55:30 +1300

    fmt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use pnet::datalink::{self, NetworkInterface};
-use pnet::packet::{Packet, ethernet::EthernetPacket};
+use pnet::packet::{ethernet::EthernetPacket, Packet};
 use tui::backend::CrosstermBackend;
+use tui::layout::{Constraint, Direction, Layout};
+use tui::widgets::{Block, Borders, Widget};
 use tui::Terminal;
-use tui::widgets::{Widget, Block, Borders};
-use tui::layout::{Layout, Constraint, Direction};
 
 use std::env;
 
@@ -12,13 +12,16 @@ fn main() {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend).unwrap();
 
-    let interface_name = env::args().nth(1).expect("Usage: capture-rs <interface_name>");
+    let interface_name = env::args()
+        .nth(1)
+        .expect("Usage: capture-rs <interface_name>");
     let interface_names_match = |iface: &NetworkInterface| iface.name == interface_name;
     let interfaces = datalink::interfaces();
-    let interface = interfaces.into_iter()
-                          .filter(interface_names_match)
-                          .next()
-                          .expect("Network interface not found");
+    let interface = interfaces
+        .into_iter()
+        .filter(interface_names_match)
+        .next()
+        .expect("Network interface not found");
 
     let (_tx, mut rx) = match datalink::channel(&interface, Default::default()) {
         Ok(datalink::Channel::Ethernet(tx, rx)) => (tx, rx),
@@ -28,13 +31,10 @@ fn main() {
 
     loop {
         match rx.next() {
-            Ok(packet) => {
-match EthernetPacket::new(packet) {
-    Some(pkt) => draw(&mut terminal, &pkt),
-    None => eprintln!("Failed to parse Ethernet packet"),
-}
-
-                            }
+            Ok(packet) => match EthernetPacket::new(packet) {
+                Some(pkt) => draw(&mut terminal, &pkt),
+                None => eprintln!("Failed to parse Ethernet packet"),
+            },
             Err(e) => {
                 panic!("An error occurred while reading the packet: {}", e);
             }
@@ -43,12 +43,11 @@ match EthernetPacket::new(packet) {
 }
 
 fn draw(terminal: &mut Terminal<CrosstermBackend<std::io::Stdout>>, packet: &EthernetPacket) {
-    terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default()
-             .title("Packet")
-             .borders(Borders::ALL);
-        f.render_widget(block, size);
-    }).unwrap();
+    terminal
+        .draw(|f| {
+            let size = f.size();
+            let block = Block::default().title("Packet").borders(Borders::ALL);
+            f.render_widget(block, size);
+        })
+        .unwrap();
 }
-
